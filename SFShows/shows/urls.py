81593f1339b5c20b30fa506a55fba67629710ca2
Rogelio Cantu <rogeliofcj@gmail.com>
2021-05-08 15:20:25 -0500

    Added basic index page and updated views

--- SFShows/shows/urls.py
+++ SFShows/shows/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+# Define app name to set application_namespace
+app_name = 'shows'
+
 #Mapping views to urls
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:venue_id>/', views.venue, name='venue'),
+    path('<int:venue_id>/venue_results/', views.venue_results, name='venue_results'),
+    path('<int:venue_id>/show/', views.show, name='show'),
+    path('<int:venue_id>/', views.detail, name='detail'),
 ]
--- SFShows/shows/views.py
+++ SFShows/shows/views.py
@@ -1,7 +1,34 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Venue
 # Create your views here.
+
+# Creating a view for 404 response error
+def detail(request, venue_id):
+    venue = get_object_or_404(Venue, pk=venus_id)
+    return render(request, 'shows/detail.html', {'venue': venue})
+
+# Creating a view for venues
+def venue(request, venue_id):
+    return HttpResponse("Looking at the venue %s" % venue_id)
+
+# Creating a view for venue responses
+def venue_results(request, venue_id):
+    response = "Looking at results for venur %s"
+    return HttpResponse(response % venue_id)
+
+# Creating a view for a show
+def show(request, venue_id):
+    return HttpResponse(
+        "Looking at a show at venue %s" % venue_id
+    )
 
 # Creating a view called index
 def index(request):
-    return HttpResponse("Hello world, you are looking for shows.")
+    latest_venue_list = Venue.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_venue_list': latest_venue_list,
+    }
+    return render(request, 'shows/index.html', context)
